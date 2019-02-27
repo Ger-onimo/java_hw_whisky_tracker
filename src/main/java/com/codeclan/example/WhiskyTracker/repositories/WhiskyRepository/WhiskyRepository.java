b557256d5dcf47c09066058ede8643369649b2ac
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-02-27 16:27:55 +0000

    Find all whiskies for a specific year - passed

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -3,5 +3,14 @@ package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRepositoryCustom {
+
+    //TODO: find whisky by given year.
+    // Have: Whiskies
+    // Want: Year
+
+    List<Whisky> findWhiskyByYear(int year);
+
 }
